f4fc2ae147b13ee43a003b07e10d1b5e1d520df1
bradstrausbaugh <bradstraus@hotmail.com>
2020-09-16 14:02:53 -0400

    Initial commit

---
+++ HelloMavenGit/src/main/java/com/bradstrausbaugh/HelloMavenGit/App.java
@@ -0,0 +1,13 @@
+package com.bradstrausbaugh.HelloMavenGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, Maven and Git!" );
+    }
+}
